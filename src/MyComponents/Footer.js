1322ca69ec686a4d5b211b88a33a5d4f746efc17
Akash Kumar <akashkumar59200@gmail.com>
2021-06-30 00:11:19 +0530

    commit on master

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+    
+    return (
+        <footer className="bg-dark text-light py-3">
+            <p className="text-center my-3">
+                Copyright &copy; MyTodoslist.com
+            </p>
+        </footer>
+    )
+}
