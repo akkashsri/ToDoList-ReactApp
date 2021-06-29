b648b2ead25493b3b3ea0f483dd36b62ba10fe31
Akash Kumar <akashkumar59200@gmail.com>
2021-06-30 00:33:18 +0530

    commit on master

--- src/App.js
+++ src/App.js
@@ -29,13 +29,13 @@ function App() {
     setTodos(todos.filter((e) => {
       return e !== todo;
     }));
-    localStorage.getItem("todos", JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
   }
 
   const addTodo = (title, desc) => {
     console.log("I am adding this todo", title, desc);
     let sno;
-    if (todos.length == 0)
+    if (todos.length === 0)
       sno = 0;
     else
       sno = todos[todos.length - 1].sno + 1;
--- src/MyComponents/AddTodo.js
+++ src/MyComponents/AddTodo.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-export const AddTodo = (props) => {
+export const AddTodo = ({ addTodo }) => {
     const [title, setTitle] = useState("");
     const [desc, setDesc] = useState("");
     const submit = (e) => {
@@ -9,10 +8,11 @@ export const AddTodo = (props) => {
         if (!title || !desc) {
             alert("Title or Description cannot be blank");
         }
-        else
-            props.addTodo(title, desc);
+        else {
+            addTodo(title, desc);
         setTitle("");
         setDesc("");
+    }
 
     }
 
